5b4853f9ff2bc76c32c7e2e7e0b08bd880d8a074
vahdetprlk <vahdetparlak@gmail.com>
2022-12-14 16:40:57 +0300

    strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: vparlak <vparlak@student.42kocaeli.com.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/13 19:47:57 by vparlak           #+#    #+#             */
-/*   Updated: 2022/12/13 19:53:30 by vparlak          ###   ########.fr       */
+/*   Updated: 2022/12/14 16:40:07 by vparlak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,18 @@
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
+	size_t	srcsize;
+	size_t	len;
 
+	srcsize = ft_strlen(src);
+	if (dstsize)
+	{
+		if (srcsize >= dstsize)
+			len = dstsize - 1;
+		else
+			len = srcsize;
+		ft_memcpy(dst, src, len);
+		dst[len] = '\0';
+	}
+	return (srcsize);
 }
--- main.c
+++ main.c
@@ -6,18 +6,52 @@
 /*   By: vparlak <vparlak@student.42kocaeli.com.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/10 17:29:05 by vparlak           #+#    #+#             */
-/*   Updated: 2022/12/12 16:35:44 by vparlak          ###   ########.fr       */
+/*   Updated: 2022/12/14 16:09:53 by vparlak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-#include <ctype.h>
 #include <stdio.h>
+#include <string.h>
+
+#include "libft.h"
+ #include "libft.h"
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	char		*tmp;
+	const char	*s;
+
+	tmp = dst;
+	s = src;
+	while (n--)
+		*tmp++ = *s++;
+	return (dst);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	srcsize;
+	size_t	len;
+
+	srcsize = strlen(src);
+	if (dstsize)
+	{
+		if (srcsize >= dstsize)
+			len = dstsize - 1;
+		else
+			len = srcsize;
+		ft_memcpy(dst, src, len);
+		dst[len] = '\0';
+	}
+	return (srcsize);
+}
+
 
 int	main(void)
 {
-	char dst[] = "Ali";
-
-	printf("%s\n", dst);
-	printf("%s\n", ft_memmove(dst, "mehmet", 3));
+	char src[] = "123456789";
+	char dst[5];
+	int a = strlcpy(dst,src,5);
+	while(a--)
+	//	printf("%c",*dst++);
 }
